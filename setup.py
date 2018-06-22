35b3637bac8fc3eec70087332f7799cf412307d7
Jack Robison <jackrobison@lbry.io>
2018-06-22 19:15:56 -0400

    Bump version 1.0.20rc6 --> 1.0.20rc7
    Signed-off-by: Jack Robison <jackrobison@lbry.io>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = [
     'jsonrpclib',
     'python-bitcoinrpc==0.1',
     'appdirs==1.4.3',
-    'lbryschema==0.0.16rc3',
+    'lbryschema==0.0.16rc4',
     'beaker==1.9.0',
     'msgpack==0.5.6',
     'twisted==16.6.0'
